352791013b85e87bda5d7135d5f0cb61ffb723ee
RahilBux <rahilbux@gmail.com>
2024-06-06 14:50:24 +0200

    task 16 fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,8 +37,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	result = is_perfect(tree);
-	if (result != 0)
-		return (1);
-	return (0);
+	else
+	{
+		result = is_perfect(tree);
+		if (result != 0)
+			return (1);
+		return (0);
+	}
 }
